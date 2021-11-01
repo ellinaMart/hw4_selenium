7d534c2c4ee83db315f1f7307502e97eb5ffac25
ellinaMart <ellina.martyn@gmail.com>
2021-11-01 04:57:23 +0200

    tests work with page object

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -1,34 +1,38 @@
 from page_objects.MainPage import MainPage
 from page_objects.UserPage import UserPage
-from page_objects.LoginAdminPage import LoginAdminPage
+from page_objects.AdminPage import LoginAdminPage
 # from page_objects.ProductPage import ProductPage
 # from page_objects.ComparisonPage import ComparisonPage
 # from page_objects.CartPage import CartPage
 from page_objects.elements.SuccessAlert import SuccessAlert
 
-def test_admin_login_page(browser):
+
+def test_admin_add_product(browser):
     browser.open("/admin")
     LoginAdminPage(browser) \
-        .login_user(login="test2@mail.ru", password="test")
+        .login_user(login="test2@mail.ru", password="test") \
+        .go_to_products() \
+        .add_product_button() \
+        .fill_product_form(name="test_product", title="product_title", model="product_model")
 
-def add_product(browser):
+def test_admin_delete_product(browser):
+    browser.open("/admin")
     LoginAdminPage(browser) \
-        .go_to_products()
-    import pdb;
-    pdb.set_trace()
+        .login_user(login="test2@mail.ru", password="test") \
+        .go_to_products() \
+        .delete_product()
 
-def test_add_to_wish_list(browser):
-
-
-    product_name = MainPage(browser).get_featured_product_name(1)
-    MainPage(browser) \
-        .click_featured_product(1) \
-        .add_to_wishlist() \
-        .alert.click_login()
-    UserPage(browser) \
-        .login_user(email="test2@mail.ru", password="test") \
-        .open_wishlist() \
-        .verify_product(product_name)
+#
+#
+#     product_name = MainPage(browser).get_featured_product_name(1)
+#     MainPage(browser) \
+#         .click_featured_product(1) \
+#         .add_to_wishlist() \
+#         .alert.click_login()
+#     UserPage(browser) \
+#         .login_user(email="test2@mail.ru", password="test") \
+#         .open_wishlist() \
+#         .verify_product(product_name)
 
 # def test_add_to_wish_list(browser):
 #     product_name = MainPage(browser).click_featured_product(1)

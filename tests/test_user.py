3efd1f01b52bcad227b23398833a437ff0482cf4
ellinaMart <ellina.martyn@gmail.com>
2021-11-04 06:07:11 +0200

    tests work with page object

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,18 +1,23 @@
 import random
 from page_objects.UserPage import UserPage
+from tests.data import test_user, new_test_user
 
 
 def test_user_register(browser):
     browser.open()
     UserPage(browser) \
         .open_register_form() \
-        .fill_register_form(firstname="Ivan", lastname="Ivanov", email="ivan+%s@test.ru"%random.randint(0,20), phone="+79271234567", password="Ivan12") \
+        .fill_register_form(firstname=new_test_user['name'],
+                            lastname=new_test_user['lastname'],
+                            email=new_test_user['login'],
+                            phone=new_test_user['phone'],
+                            password=new_test_user['password']) \
         .check_account_created()
 
 def test_change_currency(browser):
     browser.open()
     UserPage(browser) \
         .open_login_form() \
-        .login_user(email="ivan@test.ru", password="Ivan12") \
+        .login_user(email=test_user['login'], password=test_user['password']) \
         .change_currency()
 
--- tests/testdata.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-from model.letter import Letter
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + ''.join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-email_body = random_string("text", 20) + ' ' + random_string("text", 20)
